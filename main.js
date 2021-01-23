d95d096dfde5b6b3e3795c76a962b11c97a83bb3
dancyates <dcy501@york.ac.uk>
2021-01-23 00:34:19 +0000

    :sparkles: Add modal for additional movie information

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ function movieButtonClicked(movie_id) {
 const url = "https://gist.githubusercontent.com/jakerb/03e24f09aef4230eaeb98136e822599a/raw/a05b75ecd50d743ef1700a15cb3508d8a00541db/movies.json";        
 async function getData() {
     const response = await fetch(url);
-    const data = await response.json();         
+    const data = await response.json();
+
+    console.log("-----");
+    console.log(data[0]);
+    console.log("-----");
     
     let movies_results = [];
     let search = document.getElementById("search").value;
@@ -28,28 +32,29 @@ async function getData() {
                         <h5 class="card-title">${movies_results[i].title}</h5>
                         <h6 class="card-subtitle">${movies_results[i].year}</h5>
                         <p class="card-text">${movies_results[i].info.plot}</p>
-                        <button id=${i} class="btn btn-primary" data-toggle="modal" data-target=${"modal" + i}>More info</button>
+                        <button id=${i} type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target=${"#modal" + i}>More info</button>
                     </div>
                     
-                    <div class="modal fade" id=${"modal" + i} tabindex="-1" role="dialog" aria-labelledby=${"modal" + i + "label"} aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content">
+                    <div class="modal fade" id=${"modal" + i} tabindex="-1" aria-labelledby=${"modal" + i + "label"} aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
                             <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                                </button>
+                                <h5 class="modal-title" id=${"modal" + i + "label"}>${movies_results[i].title}</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div class="modal-body">
-                                Modal body goes into here example 123.
+                                <ul>
+                                    <li>Released: ${movies_results[i].year}</li>
+                                    <li>Rating: ${movies_results[i].info.rating}/10</li>
+                                    <li>Plot: ${movies_results[i].info.plot}</li>
+                                </ul>
                             </div>
                             <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Save changes</button>
-                            </div>
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                             </div>
                         </div>
                     </div>
+                </div>
                 </div>
             </div>
         `
